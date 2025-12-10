c24cf3d44371cbd3f0c9b695cfcc9df83f4d6e1c
aarafqita <aaqi07310@gmail.com>
2025-12-10 23:52:39 +0300

    Form and pop push

--- lib/main.dart
+++ lib/main.dart
@@ -2,102 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:listing/secand.dart';
 
 void main() {
-  runApp(ProfileApp());
+  runApp(const MyApp());
 }
 
-class ProfileApp extends StatefulWidget {
-  @override
-  _ProfileAppState createState() => _ProfileAppState();
-}
-
-class _ProfileAppState extends State<ProfileApp> {
-  final _formKey = GlobalKey<FormState>();
-  String _name = '';
-  String _email = '';
-  final TextEditingController _passwordController = TextEditingController();
-
-
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      print('Name: $_name, Email: $_email, Password : ${_passwordController.text}');
-      Navigator.push(context,
-      MaterialPageRoute(builder: (context)=> Secand(name:_name),
-
-      ));
-    }
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Form Example')),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Name'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _name = value!;
-                  },
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    } else if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!)) {
-                      return 'Please enter a valid email address';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _email = value!;
-                  },
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    else if (value!.length<8) {
-                      return ' password should be at least 8 chars';
-                    }else if (!RegExp("[a-z]").hasMatch(value)) {
-                      return ' password should at least one lowercase chars';
-                    }else if (!RegExp("[A-Z]").hasMatch(value)) {
-                      return ' password should at least one uppercase chars';
-                    }else if (!RegExp("[!#\$%&'*+-/=?^_`{|}~@]").hasMatch(value)) {
-                      return ' password should at least one symbol';
-                    }else if (!RegExp("[0-9]").hasMatch(value)) {
-                      return ' password should at least one number';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text('Submit'),
-                ),
-              ],
-            ),
-          ),
+    return Scaffold(
+      appBar: AppBar(title:  Text('Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async{
+            final result = await Navigator.push<bool>(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>  Secand(),
+              ),
+            );
+            if (result != null) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(result ?'تمت الموافقة على الشروط':'تم رفض الشروط')),
+              );
+            }
+          },
+          child:  Text('Go to selection screen'),
         ),
       ),
     );
   }
+
 }
