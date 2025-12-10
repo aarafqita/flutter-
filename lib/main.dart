977d888b6dbf584d5dcb07971fac3da8d632da13
aarafqita <aaqi07310@gmail.com>
2025-12-10 22:54:02 +0300

    ToDo

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TodoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TodoApp extends StatefulWidget {
+  @override
+  _TodoAppState createState() => _TodoAppState();
+}
 
+class _TodoAppState extends State<TodoApp> {
+  final List<String> _todos = [];
+
+  void _addTodoItem(String task) {
+    if (task.isNotEmpty) {
+      setState(() {
+        _todos.add(task);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: Text('Product Listing '),),
-          body: ListView(
-            children: [
-              Row(
-                children: [
-                  Image.asset('images/2.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('iPhone',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                        SizedBox(height: 10,),
-                        Text('iPhone is the slylist phone ever'),
-                        SizedBox(height: 10,),
-                        Text('price: 1000'),
-                      ],
-                    ),
-                  )
-                ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple ToDo App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: _addTodoItem,
+              decoration: InputDecoration(
+                labelText: 'Enter a new task',
               ),
-              Row(
-                children: [
-                  Image.asset('images/1.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('Pixel',style: TextStyle(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 10,),
-                        Text('Pixel is most featureful phone ever'),
-                        SizedBox(height: 10,),
-                        Text('price: 800'),
-                      ],
-                    ),
-                  )
-                ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _todos.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_todos[index]),
+                  );
+                },
               ),
-              Row(
-                children: [
-                  Image.asset('images/3.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('Laptop',style: TextStyle(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 10,),
-                        Text('Laptop is most productive development tool'),
-                        SizedBox(height: 10,),
-                        Text('price:2000'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset('assets/1.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('Tablet',style: TextStyle(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 10,),
-                        Text('Tablet the most useful device ever for meetinfg '),
-                        SizedBox(height: 10,),
-                        Text('price: 1500'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset('images/4.jpg',width:100,height: 100,),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-
-                        Text('Pen Drive',style: TextStyle(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 10,),
-                        Text('iPhone is the slylist phone ever'),
-                        SizedBox(height: 10,),
-                        Text('price: 6000'),
-
-
-                      ],
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        )
-
+            ),
+          ],
+        ),
+      ),
     );
-  }}
+  }
+}
